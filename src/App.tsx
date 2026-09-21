07926ede617f08459638f05b0abb6bcec3c86998
Thesanya <thesanyawijayasundara490@gmail.com>
2026-09-21 16:59:19 +0530

    Add routes for login and token display

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { AccessTokenPage } from './pages/AccessTokenPage';
 import { LoginPage } from './pages/LoginPage';
 
 function App() {
-  return <LoginPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/access-token" element={<AccessTokenPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
